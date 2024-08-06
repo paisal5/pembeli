1d266cc272ad51c435b5d4784163037bcbe1c285
paisal5 <iyattpaisal@gmail.com>
2024-08-07 06:24:47 +0700

    update main.js

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ import {
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBlahoJjeK0jyO-4tZlAiPRjym6Mxn2P6o",
-  authDomain: "insan-cemerlang-59727.firebaseapp.com",
-  projectId: "insan-cemerlang-59727",
-  storageBucket: "insan-cemerlang-59727.appspot.com",
-  messagingSenderId: "839220708273",
-  appId: "1:839220708273:web:4d1dde85cf74aebd1d7390",
-  measurementId: "G-1VP3D59R0T"
+  apiKey: "AIzaSyAwZk_BSxdm6_VJsVPI2Ne8S3RO5pi0-lg",
+  authDomain: "paisal-abret.firebaseapp.com",
+  projectId: "paisal-abret",
+  storageBucket: "paisal-abret.appspot.com",
+  messagingSenderId: "368318578592",
+  appId: "1:368318578592:web:491e88e8b6eee503d72ec5",
+  measurementId: "G-7Q534CCZNV"
 };
 
 // Inisialisasi Firebase
